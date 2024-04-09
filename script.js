bdf2f43f69855a77be84e6ab2763b420ae09ca1e
Sergio-Issao <160669611+Sergio-Issao@users.noreply.github.com>
2024-04-09 17:03:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var numeroParaEncontrar = 0;
+var tentativas = 0;
+
+function atualizar() {
+    var numeroDoInput = document.getElementById ('inputNumero');
+
+    numeroDoInput.value = '' 
+
+    numeroParaEncontrar = parseInt (Math.random() * 100)
+    
+}
+
+function verificaNumeros(){
+
+}
